a53d8177cbf2069e34301bceb7610e19b4524ef5
emchase8 <emchase8@byu.edu>
2026-02-23 16:43:54 -0700

    post status tests mostly done

--- tweeter-web/test/presenter/LogoutPresenter.test.ts
+++ tweeter-web/test/presenter/LogoutPresenter.test.ts
@@ -21,41 +21,41 @@ describe("LogoutPresenter", () => {
   let mockLogoutPresenterView: LogoutView;
   let mockLogoutPresenterViewInstance: LogoutView;
   let logoutPresenterSpy: LogoutPresenter;
-  let logoutPresenter: LogoutPresenter;
+  let logoutPresenterSpyInstance: LogoutPresenter;
   let mockService: UserService;
   const authToken = new AuthToken("ProjectStardust", Date.now());
 
   beforeEach(() => {
     mockLogoutPresenterView = mock<LogoutView>();
     when(mockLogoutPresenterView.displayInfoMessage(anything(), 0)).thenReturn(
-      "a wonderful message",
+      "deploy the garrison",
     );
     mockLogoutPresenterViewInstance = instance(mockLogoutPresenterView);
     logoutPresenterSpy = spy(
       new LogoutPresenter(mockLogoutPresenterViewInstance),
     );
-    logoutPresenter = instance(logoutPresenterSpy);
+    logoutPresenterSpyInstance = instance(logoutPresenterSpy);
     mockService = mock<UserService>();
     when(logoutPresenterSpy.service).thenReturn(instance(mockService));
   });
 
   it("tells the view to display a logging out message", async () => {
-    await logoutPresenter.logOut(authToken);
+    await logoutPresenterSpyInstance.logOut(authToken);
     verify(
       mockLogoutPresenterView.displayInfoMessage("Logging Out...", 0),
     ).once();
   });
 
   it("calls logout on the user service with the correct auth token", async () => {
-    await logoutPresenter.logOut(authToken);
+    await logoutPresenterSpyInstance.logOut(authToken);
     verify(mockService.logout(authToken)).once();
     let [capturedAuthToken] = capture(mockService.logout).last();
     expect(capturedAuthToken).toEqual(authToken);
   });
 
   it("when logout successful, clear the info message that was displayed previously, clear the user info, and navigates to the login page", async () => {
-    await logoutPresenter.logOut(authToken);
-    verify(mockLogoutPresenterView.deleteMessage("a wonderful message")).once();
+    await logoutPresenterSpyInstance.logOut(authToken);
+    verify(mockLogoutPresenterView.deleteMessage("deploy the garrison")).once();
     verify(mockLogoutPresenterView.clearUserInfo()).once();
     verify(mockLogoutPresenterView.navigate("/login")).once();
     verify(mockLogoutPresenterView.displayErrorMessage(anything())).never();
@@ -64,11 +64,7 @@ describe("LogoutPresenter", () => {
   it("when logout not successful, display an error message and does not tell it to clear the info message, clear the user info or navigate to the login page", async () => {
     let error = new Error("its a trap");
     when(mockService.logout(anything())).thenThrow(error);
-    await logoutPresenter.logOut(authToken);
-    let errorString = capture(
-      mockLogoutPresenterView.displayErrorMessage,
-    ).last();
-    console.log(errorString);
+    await logoutPresenterSpyInstance.logOut(authToken);
     verify(
       mockLogoutPresenterView.displayErrorMessage(
         `Failed to log user out because of exception: ${error.message}`,
---
+++ tweeter-web/test/presenter/PostStatusPresenter.test.ts
@@ -0,0 +1,81 @@
+import {
+  instance,
+  mock,
+  verify,
+  spy,
+  when,
+  capture,
+  anything,
+} from "@typestrong/ts-mockito";
+import {
+  PostStatusPresenter,
+  PostStatusView,
+} from "../../src/presenter/PostStatusPresenter";
+import { StatusService } from "../../src/model.service/StatusService";
+import { AuthToken, Status, User } from "tweeter-shared";
+
+describe("PostStatusPresenter", () => {
+  let mockPostStatusView: PostStatusView;
+  let mockPostStatusViewInstance: PostStatusView;
+  let postStatusPresenterSpy: PostStatusPresenter;
+  let postStatusPresenterSpyInstance: PostStatusPresenter;
+  let mockService: StatusService;
+  const authToken = new AuthToken("ProjectTieDefender", Date.now());
+  const user = new User(
+    "Mitth'raw'nuruodo",
+    "Grand Admiral",
+    "Thrawn",
+    "/path/to/image",
+  );
+  const post = "I like art";
+
+  beforeEach(() => {
+    mockPostStatusView = mock<PostStatusView>();
+    when(mockPostStatusView.displayInfoMessage(anything(), 0)).thenReturn("Nonsense");
+    mockPostStatusViewInstance = instance(mockPostStatusView);
+    postStatusPresenterSpy = spy(
+      new PostStatusPresenter(mockPostStatusViewInstance),
+    );
+    postStatusPresenterSpyInstance = instance(postStatusPresenterSpy);
+    mockService = mock<StatusService>();
+    when(postStatusPresenterSpy.service).thenReturn(instance(mockService))
+  });
+
+  it("tells the view to display a posting status message", async () => {
+    await postStatusPresenterSpyInstance.submitPost(user, authToken, post);
+    verify(
+      mockPostStatusView.displayInfoMessage("Posting status...", 0),
+    ).once();
+  });
+
+  it("calls postStatus on the post status service with the correct status string and auth token", async () => {
+    await postStatusPresenterSpyInstance.submitPost(user, authToken, post);
+    let [capturedAuth, capturedStatus] = capture(mockService.postStatus).last();
+    expect(capturedAuth).toEqual(authToken);
+    expect(capturedStatus.post).toBe("I like art");
+  });
+
+  it("When posting status successful, clear the info message that was displayed previously, clear the post, and display a status posted message", async () => {
+    await postStatusPresenterSpyInstance.submitPost(user, authToken, post);
+    verify(mockPostStatusView.deleteMessage("Nonsense")).once();
+    verify(mockPostStatusView.setPost("")).once();
+    verify(mockPostStatusView.displayInfoMessage("Status posted!", 2000)).once();
+    verify(mockPostStatusView.displayErrorMessage(anything())).never();
+  });
+
+  //what does it mean by "clear the info message"? as far as I can tell, that only happens if the post was successful
+  it("When posting status not successful, clear the info message and display an error message but does not tell it to clear the post or display a status posted message", async () => {
+    let error = new Error("I have miscalculated");
+    when(mockService.postStatus(anything(), anything())).thenThrow(error);
+    await postStatusPresenterSpyInstance.submitPost(user, authToken, post);
+    verify(
+      mockPostStatusView.displayErrorMessage(
+        `Failed to post the status out because of exception: ${error.message}`,
+      ),
+    );
+    verify(mockPostStatusView.setPost("")).never();
+    verify(
+      mockPostStatusView.displayInfoMessage("Status posted!", 2000),
+    ).never();
+  });
+});
